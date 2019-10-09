a98e89404038e44d88d2c1909d199700f28c0a49
Ilia Idakiev <ilia.idakiev@gmail.com>
2019-10-09 19:39:50 +0300

    task

--- week2/1/fs.js
+++ week2/1/fs.js
@@ -1,5 +1,9 @@
 var fs = require('fs');
 
+
+// Task: Read studendts.txt file and calculate for each individual student his average scores
+// After that seave all results into result.txt file with the following format: 
+// <FN_NUMBER> <AVG_SCORE>
 fs.readFile('./students.txt', { encoding: 'utf-8' }, function (err, data) {
   console.log(data);
 });
