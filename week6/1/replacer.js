b0479218c7d6862157341e5cb61235d20bdbedf2
Ilia Idakiev <ilia.idakiev@gmail.com>
2019-11-06 20:53:09 +0200

    magic 2

---
+++ week6/1/replacer.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const { Transform } = require('stream');
+
+const readStream = fs.createReadStream('./text.txt', { highWaterMark: 10 });
+const writeStream = fs.createWriteStream('./revision.txt');
+
+class RevisionStream extends Transform {
+
+  constructor(opts) {
+    super({ ...opts });
+    this.rest = null;
+    this.dictionary = ['Lorem', 'Ipsum'];
+  }
+
+  _transform(chunk, enc, next) {
+    chunk = chunk.toString();
+    chunk = this.rest ? this.rest + chunk : chunk;
+    if (this.rest) { this.rest = ''; }
+    if (chunk[chunk.length - 1] !== ' ') {
+      const lastIndex = chunk.lastIndexOf(' ');
+      this.rest = lastIndex === -1 ? chunk : chunk.slice(lastIndex, chunk.length);
+      chunk = lastIndex === -1 ? '' : chunk.slice(0, lastIndex);
+    }
+    if (!chunk) { return; }
+    next(null, this.revision(chunk));
+  }
+
+  _flush() {
+    this.push(this.revision(this.rest));
+  }
+
+  revision(string) {
+    const pattern = this.dictionary.join('|');
+    return string.replace(new RegExp(pattern, 'gi'), val => '*'.repeat(val.length));
+  }
+}
+
+const re = new RevisionStream();
+
+readStream.pipe(re).pipe(writeStream);
